d699e0140d66ed6e2806daa491ea9d366eec376d
David Hayes <drhayes@gmail.com>
2021-04-15 22:35:15 -0500

    Do a heartbeat check to ensure bot is running.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const got = require('got');
+const { setIntervalAsync } = require('set-interval-async/fixed');
 
-const { DISCORD_BOT_TOKEN } = process.env;
+const { DISCORD_BOT_TOKEN, HEARTBEAT_HEALTHCHECKS_IO_URL } = process.env;
+
+setIntervalAsync(async () => {
+  // Send a heartbeat request to our healtcheck so it knows we're alive.
+  try {
+    await got.post(HEARTBEAT_HEALTHCHECKS_IO_URL, {
+      body: '💙',
+    }) catch (error) {
+      console.error(`error:heartbeat`, error);
+    };
+  }
+}, 5 * 60 * 1000);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
