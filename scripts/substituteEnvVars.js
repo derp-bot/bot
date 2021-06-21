bda06b5e78b70dade59ccf4a4bd044d48e9a50bc
David Hayes <drhayes@gmail.com>
2021-06-21 11:43:53 -0500

    Script for updating downloaded task definition.

---
+++ scripts/substituteEnvVars.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const envVarNames = [
+  'DISCORD_BOT_TOKEN',
+  'HEARTBEAT_HEALTHCHECKS_IO_URL'
+];
+
+const deleteTheseKeys = [
+  'taskDefinitionArn',
+  'revision',
+  'status',
+  'requiresAttributes',
+  'compatibilities',
+]
+
+// Load our task def.
+const taskDefinitionPath = path.relative(__dirname, path.resolve(process.argv[process.argv.length - 1]));
+const taskDefinition = require(taskDefinitionPath);
+
+// Set our environment variables.
+const environmentVars = taskDefinition.environment || [];
+const newEnvironmentVars = environmentVars.concat(envVarNames.map(name => ({
+  name,
+  value: process.env[name],
+})));
+taskDefinition.environment = newEnvironmentVars;
+
+// Delete the values we should'nt be sending back.
+deleteTheseKeys.forEach(key => {
+  delete taskDefinition[key];
+});
+
+console.log(JSON.stringify(taskDefinition, null, 2));
+
+// await fs.writeFile(taskDefinitionPath, JSON.stringify(taskDefinition, null, 2));
