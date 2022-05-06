183d82b80caa2b60fece156333ce8f71cd61c181
David Hayes <drhayes@hey.com>
2022-05-06 00:04:22 -0500

    Add a botinfo command to return the tiniest bit of information.

---
+++ src/commands/botinfo.js
@@ -0,0 +1,17 @@
+const { registerCommand } = require('./index');
+const { GITHUB_SHA, GITHUB_BRANCH } = require('../config');
+const { inlineCode, hyperlink } = require('@discordjs/builders');
+
+registerCommand(
+  'botinfo',
+  'Replies with some debugging info about the bot.',
+  async (msg) => {
+    const shaLink = hyperlink(inlineCode(GITHUB_SHA), `https://github.com/derp-bot/bot/commit/${GITHUB_SHA}`);
+
+    await msg.payload.interaction.reply({
+      content: `Derp the Bot!
+sha: ${shaLink}`,
+      ephemeral: true,
+    });
+  });
+
