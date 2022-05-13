0e8680da315e7961e15ea4410e771475eb60cc79
David Hayes <drhayes@hey.com>
2022-05-13 16:57:06 -0500

    Adjust intents.

--- src/derp.js
+++ src/derp.js
@@ -9,6 +9,12 @@ export class Derp extends Client {
   constructor() {
     super({
       intents: [
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
         Intents.FLAGS.GUILDS,
       ]
     });
